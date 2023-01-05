edf1d2bee7cbe591e38d0e197442ed00fddd642b
Dmytro Shakh <work.dmytro02@gmail.com>
2023-01-05 23:07:33 +0100

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+const alphabet = katakana + latin + nums;
+
+export {
+  katakana,
+  latin,
+  nums,
+  alphabet,
+};
